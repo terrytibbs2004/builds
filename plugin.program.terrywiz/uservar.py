a97947b2abd1ac120445c2e3a6849404e883dec3
terrytibbs2004 <118140781+terrytibbs2004@users.noreply.github.com>
2022-11-14 12:38:30 +0000

    icontest

---
+++ plugin.program.terrywiz/uservar.py
@@ -0,0 +1,12 @@
+import xbmcaddon
+
+addon_id = xbmcaddon.Addon().getAddonInfo('id')
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://githubraw.com/terrytibbs2004/builds/main/builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://githubraw.com/terrytibbs2004/builds/main/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = [addon_id, 'packages', 'Addons33.db', 'kodi.log', 'script.module.certifi', 'script.module.chardet', 'script.module.idna', 'script.module.requests', 'script.module.urllib3', 'backups', 'plugin.video.whatever']
