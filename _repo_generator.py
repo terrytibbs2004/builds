4c9d0a0df693b87ac8c6a3ae57c22b28420619d9
terry <terrytibbskodi@gmail.com>
2022-12-21 12:45:26 +0000

    h

--- _repo_generator.py
+++ _repo_generator.py
@@ -13,7 +13,7 @@ import zipfile
 from xml.etree import ElementTree
 
 SCRIPT_VERSION = 5
-KODI_VERSIONS = ["krypton", "leia", "matrix", "nexus", "repo"]
+KODI_VERSIONS = ["krypton", "leia", "matrix", "nexus", "repo2"]
 IGNORE = [
     ".git",
     ".github",
